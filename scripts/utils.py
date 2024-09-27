afcdd8832270bdc8d6040e1ed0e5353666528ea9
availe <94802525+availe@users.noreply.github.com>
2024-09-27 18:14:20 -0400

    Delete utils.py

--- scripts/utils.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from pathlib import Path
-
-def load(filename, folder_path='/content/Spystat-Engine-Colab/data/clean/'):
-    # Load the file directly
-    file_path = Path(folder_path) / filename
-    return pd.read_csv(file_path) if file_path.exists() else print(f"{filename} not found.")
