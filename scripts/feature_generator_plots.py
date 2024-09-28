2fa7767e41742943075527c1cbe6d6e39b7c3ff0
availe <94802525+availe@users.noreply.github.com>
2024-09-27 21:05:01 -0400

    Update feature_generator_plots.py

--- scripts/feature_generator_plots.py
+++ scripts/feature_generator_plots.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import ipywidgets as widgets
 
 def plot_spy_moving_averages(data):
-    plt.figure(figsize=(10, 6))
+    fig = plt.figure(figsize=(10, 6))
     plt.plot(data['Date'], data['SPY_Close'], label='SPY Close', color='blue')
     plt.plot(data['Date'], data['SPY_5_MA_Close'], label='5-Day MA', color='orange', linestyle='--')
     plt.plot(data['Date'], data['SPY_20_MA_Close'], label='20-Day MA', color='green', linestyle='--')
@@ -10,10 +10,10 @@ def plot_spy_moving_averages(data):
     plt.ylabel('Price')
     plt.title('SPY Close Prices with 5-Day and 20-Day Moving Averages')
     plt.legend()
-    plt.show()
+    return fig
 
 def plot_spy_oscillators_and_volume_z_scores(data):
-    plt.figure(figsize=(12, 8))
+    fig = plt.figure(figsize=(12, 8))
     plt.plot(data['Date'], data['SPY_Oscillator_5'], label='SPY 5-Day Oscillator', color='blue', linestyle='--')
     plt.plot(data['Date'], data['SPY_Oscillator_20'], label='SPY 20-Day Oscillator', color='green', linestyle='--')
     plt.plot(data['Date'], data['SPY_Volume_Z_5'], label='5-Period Z-Score of SPY Volume', color='red', alpha=0.6)
@@ -23,37 +23,37 @@ def plot_spy_oscillators_and_volume_z_scores(data):
     plt.title('SPY Oscillators and Volume Z-Scores')
     plt.legend()
     plt.grid(True)
-    plt.show()
+    return fig
 
 def plot_spy_percent_close_change(data):
-    plt.figure(figsize=(12, 6))
+    fig = plt.figure(figsize=(12, 6))
     plt.plot(data['Date'], data['SPY_Percent_Close_Change'], label="Percent Change in SPY Close", color='red')
     plt.title('Percent Change in SPY Close Price Over Time')
     plt.xlabel('Date')
     plt.ylabel('Percent Change (%)')
     plt.legend()
     plt.grid(True)
-    plt.show()
+    return fig
 
 def plot_spy_percent_high_to_previous_close(data):
-    plt.figure(figsize=(10, 6))
+    fig = plt.figure(figsize=(10, 6))
     plt.plot(data['Date'], data['SPY_Percent_Change:High_To_Previous_Close'], label="SPY Percent High Change", color='red')
     plt.title('Percent Change of High to Previous Close Over Time')
     plt.xlabel('Date')
     plt.ylabel('Percent Change (%)')
     plt.legend()
     plt.grid(True)
-    plt.show()
+    return fig
 
 def plot_spy_percent_low_to_previous_close(data):
-    plt.figure(figsize=(10, 6))
+    fig = plt.figure(figsize=(10, 6))
     plt.plot(data['Date'], data['SPY_Percent_Change:Low_To_Previous_Close'], label="SPY Percent Low Change", color='red')
     plt.title('Percent Change of Low to Previous Close Over Time')
     plt.xlabel('Date')
     plt.ylabel('Percent Change (%)')
     plt.legend()
     plt.grid(True)
-    plt.show()
+    return fig
 
 def fg_plot_all(data):
     tab1 = widgets.Output()
@@ -63,15 +63,25 @@ def fg_plot_all(data):
     tab5 = widgets.Output()
 
     with tab1:
-        plot_spy_moving_averages(data)
+        fig = plot_spy_moving_averages(data)
+        display(fig)
+        plt.close(fig)
     with tab2:
-        plot_spy_oscillators_and_volume_z_scores(data)
+        fig = plot_spy_oscillators_and_volume_z_scores(data)
+        display(fig)
+        plt.close(fig)
     with tab3:
-        plot_spy_percent_close_change(data)
+        fig = plot_spy_percent_close_change(data)
+        display(fig)
+        plt.close(fig)
     with tab4:
-        plot_spy_percent_high_to_previous_close(data)
+        fig = plot_spy_percent_high_to_previous_close(data)
+        display(fig)
+        plt.close(fig)
     with tab5:
-        plot_spy_percent_low_to_previous_close(data)
+        fig = plot_spy_percent_low_to_previous_close(data)
+        display(fig)
+        plt.close(fig)
 
     tab = widgets.Tab(children=[tab1, tab2, tab3, tab4, tab5])
     tab.set_title(0, 'Moving Averages')
